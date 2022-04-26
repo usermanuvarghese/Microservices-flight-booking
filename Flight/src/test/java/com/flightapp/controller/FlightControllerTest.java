188641ce98eb4baaf1aec2cd54b8ceaf08e9047c
Manu <manu.arikat96@gmail.com>
2022-04-26 11:35:01 +0530

    update

--- Flight/src/test/java/com/flightapp/controller/FlightControllerTest.java
+++ Flight/src/test/java/com/flightapp/controller/FlightControllerTest.java
@@ -2,16 +2,41 @@ package com.flightapp.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.function.Executable;
+import com.flightapp.exception.FlightNotFoundException;
+import com.flightapp.model.Flight;
+
+@SpringBootTest
 class FlightControllerTest {
 
+	@Autowired
+	private FlightController flightController;
+
+	@Test
+	void searchFlightTest() throws FlightNotFoundException {
+
+		String from = "Singapore";
+		String to = "Mumbai";
+		String depDate = "2022-04-28";
+		assertEquals("AIRINDIA", flightController.searchFlight(from, to, depDate).get(0).getAirlineName());
+
+	}
+	
 	/*
-	 * @Autowired
+	 * @Test public void searchFlightTestInvalid() {
 	 * 
-	 * @Test void searchFlightTest() {
+	 * Assertions.assertThrows(FlightNotFoundException.class,()->{
+	 * flightController.searchFlight("a", "Singapore", "2022-04-28") });
 	 * 
 	 * }
 	 */
+	 
 
 }
